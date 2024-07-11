d743708f43eae2abc2a43803d752eeb69a14a26b
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2024-07-11 21:11:52 +0300

    change folder structure all in front

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+// Hello from the other side
+
+import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar/NavBar.jsx';
+import SearchBar from './components/SearchBar/SearchBar.jsx';
+import CategoryCard from './components/CategoryCard/CategoryCard.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs'; 
+import Services from './pages/Services';
+import Login from './pages/Login';
+
+function App() {
+  const handleSearch = (query) => {
+    console.log('Search query:', query);
+  };
+
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home handleSearch={handleSearch} />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
