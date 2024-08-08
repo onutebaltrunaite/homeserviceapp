6103f0761b4bc92ade03e69c9795941df4496073
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2024-08-09 00:37:22 +0300

    categories refactor

--- frontend/src/components/CategoryCard/CategoryCard.jsx
+++ frontend/src/components/CategoryCard/CategoryCard.jsx
@@ -1,51 +1,26 @@
 import React from 'react';
-import styles from './CategoryCard.module.scss'; // Assuming you have a CSS file for styling
-import Plumbing from '../Categories/Plumbing'; // Importing component for Plumbing
-import Electric from '../Categories/Electric'; // Importing component for Electric
-import Cleaning from '../Categories/Cleaning'; // Importing component for Cleaning
-import Painting from '../Categories/Painting'; // Importing component for Painting
-import Repair from '../Categories/Repair'; // Importing component for Repair
-import Shifting from '../Categories/Shifting'; // Importing component for Shifting
-
-// Define categories array within the same file
-const categories = [
-  { title: "Plumbing" },
-  { title: "Electric" },
-  { title: "Cleaning" },
-  { title: "Painting" },
-  { title: "Repair" },
-  { title: "Shifting" },
-];
+import styles from './CategoryCard.module.scss'; 
+import { categories } from '../../consts/categories';
 
 const CategoryCard = () => {
   return (
-    <div className={styles.categoryCard}> {/* Assuming you apply styles directly via className */}
-      {categories.map((category, index) => (
-        renderCategory(category.title, index)
+    <div className={styles.categoryCard}>
+      {categories.map((category) => (
+        <div key={category._id} className={styles.categoryItem}>
+          <img 
+            src={category.images[0].url} 
+            alt={category.name} 
+            className={styles.categoryImage} 
+          />
+          <span className={styles.categoryName}>{category.name}</span>
+        </div>
+
+        
       ))}
     </div>
   );
 };
 
-// Function to render category components dynamically
-const renderCategory = (title, key) => {
-  switch (title) {
-    case "Plumbing":
-      return <Plumbing key={key} />;
-    case "Electric":
-      return <Electric key={key} />;
-    case "Cleaning":
-      return <Cleaning key={key} />;
-    case "Painting":
-      return <Painting key={key} />;
-    case "Repair":
-      return <Repair key={key} />;
-    case "Shifting":
-      return <Shifting key={key} />;
-    default:
-      return null;
-  }
-};
 
 export default CategoryCard;
 
---
+++ frontend/src/consts/categories.js
@@ -0,0 +1,62 @@
+export const categories = [
+    {
+        _id: "1",
+        name: "Cleaning",
+        description: "Cleaning Services",
+        images: [
+          {
+            url: "https://img.icons8.com/?size=100&id=12238&format=png&color=000000",
+          },
+        ],
+      },
+      {
+        _id: "2",
+        name: "Electric",
+        description: "Electrical Services",
+        images: [
+          {
+            url: "https://img.icons8.com/?size=100&id=80432&format=png&color=000000",
+          },
+        ],
+      },
+      {
+        _id: "3",
+        name: "Painting",
+        description: "Painting Services",
+        images: [
+          {
+            url: "https://img.icons8.com/?size=100&id=65329&format=png&color=000000",
+          },
+        ],
+      },
+      {
+        _id: "4",
+        name: "Plumbing",
+        description: "Plumbing Services",
+        images: [
+          {
+            url: "https://img.icons8.com/?size=100&id=zKnFtoamppoG&format=png&color=000000",
+          },
+        ],
+      },
+      {
+        _id: "5",
+        name: "Repair",
+        description: "Repair Services",
+        images: [
+          {
+            url: "https://img.icons8.com/?size=100&id=RhJ0vJceSubS&format=png&color=000000",
+          },
+        ],
+      },
+      {
+        _id: "6",
+        name: "Shifting",
+        description: "Shifting Services",
+        images: [
+          {
+            url: "https://img.icons8.com/?size=100&id=sbV9vt0Sb5VY&format=png&color=000000",
+          },
+        ],
+      },
+];
